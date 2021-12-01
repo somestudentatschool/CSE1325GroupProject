0247b9752fb451a9fbf20562fd10ff8312e74fa4
Efaz Hossain <efaz.hossain@mavs.uta.edu>
2021-12-01 14:11:08 -0600

    Added a constructor for the Person class using a String name and int ID for the parameters.

--- src/People/People.java
+++ src/People/People.java
@@ -1,4 +1,15 @@
 package People;
 
 public class People {
+
+    //variables used to make a random person constructor
+    String name;
+    int ID;
+
+    //Person constructor using their name and ID
+    public People(String name, int ID)
+    {
+        this.name = name;
+        this.ID = ID;
+    }
 }
