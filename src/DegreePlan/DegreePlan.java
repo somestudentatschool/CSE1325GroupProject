1a57d3b48fc4dd828dbef6934fd53c758ec4b6de
Efaz Hossain <efaz.hossain@mavs.uta.edu>
2021-11-30 18:42:18 -0600

    DegreePlan/main changes
    I was discussing with Ahmed that maybe it would be better if we could make the Degrees go in an enum and we could assign values to the Degree enums and treat it like the values of students in that supposed field
    
    Also added some basic GUI stuff in the main file

--- src/DegreePlan/DegreePlan.java
+++ src/DegreePlan/DegreePlan.java
@@ -1,6 +1,28 @@
 package DegreePlan;
 
+
 public class DegreePlan
 {
+    /*Was thinking we could use an enum and get information using it,
+    like if we need to get how many students are enrolled in classes
+    and in a specific major.*/
+    public enum Degree
+    {
+        Computer_Science(1),
+        Nursing(2),
+        Mechanical_Engineering(1),
+        Accounting(3),
+        Chemistry(4);
+        private int students;
 
+        void StudentMajors(int students)
+        {
+            this.students = students;
+        }
+
+        public int getStudents()
+        {
+            return students;
+        }
+    }
 }
--- src/projectMain.java
+++ src/projectMain.java
@@ -1,11 +1,22 @@
 import DegreePlan.*;
 import Grades.*;
 import People.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class projectMain
 {
     public static void main(String[] args)
     {
+        //code GUI stuff here
+        //not finished yet - Efaz
+        JFrame project = new JFrame();
+        project.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        project.setSize(1280,720);
+        project.setVisible(true);
+
+
+        //make the GUI screen
 
     }
 }
