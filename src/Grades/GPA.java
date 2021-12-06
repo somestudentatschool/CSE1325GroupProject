cc7178b217a7579aeecc9ff7123ade5c83c04605
Efaz Hossain <efaz.hossain@mavs.uta.edu>
2021-12-06 12:37:32 -0600

    12/6/2021
    Figured a switch-case statement chain would be better than a bunch of if-else statements and looks a whole lot nicer too.  It should automatically calculate your GPA, working on adding a GUI window for the GPA Calculator page later.

--- src/Grades/GPA.java
+++ src/Grades/GPA.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class GPA
 {
     String grade = "";
-    double credit,gpa,gradeValues,points
+    String grades = "";
+    double credit,gpa,gradeValues,points;
     double totalPoints = 0;
     double totalCredits = 0;
 
@@ -12,18 +13,42 @@ public class GPA
     {
         for(int i = 0; i < 3; i++)
         {
-            Scanner enterInfo = new Scanner();
+            Scanner keyboard = new Scanner(System.in);
             System.out.println("Please enter the amount of credits for class " + i + ": ");
-            credit = enterInfo.nextDouble();
-            System.out.println("Please enter your grade for class "+ i+ ": (In letter form) ");
-            grade = enterInfo.next();
+            credit = keyboard.nextDouble();
+            System.out.println("Please enter your grade for class "+ i + ": (In letter form) ");
+            grade = keyboard.nextLine();
+            grades += grade;
         }
+        return grades;
     }
 
+    //Changed how it should check for what letter grade someone has and the points attributed to it, might be buggy
+    //just doing some rough proto-coding - Efaz
     public double calculateGPA()
     {
-        if(grade.equals("A"))
+        switch(grade)
+        {
+            case "A": case "a" : gradeValues = 4.0;
+            break;
+            case "B": case "b" : gradeValues = 3.0;
+            break;
+            case "C": case "c" : gradeValues = 2.0;
+            break;
+            case "D": case "d" : gradeValues = 1.0;
+            break;
+            case "F": case "f" : gradeValues = 0.0;
+            break;
+
+            default: gradeValues = 0;
+        }
+        return totalPoints += totalCredits*gradeValues;
     }
 
+    //will work on this later, planning to arrange the GUI page for the GPA calculator, should discuss this
+    // on  Tuesday 12/7- Efaz
+    public void GPA_GUI()
+    {
 
+    }
 }
