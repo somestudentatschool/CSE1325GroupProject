24353104cd853f5faf35d9e81eff92a78ad2c794
Efaz Hossain <efaz.hossain@mavs.uta.edu>
2021-12-07 19:19:41 -0600

    projectMain updates
    added a login screen, will add the read file to make different users after the .text files are done

--- src/projectMain.java
+++ src/projectMain.java
@@ -9,12 +9,67 @@ class selectListener implements ActionListener { //added this for the combo box 
     public void actionPerformed(ActionEvent e) {
 
     }
-    }
+}
+
+    //Added the login screen with GUI implementation
+    class loginScreen extends JFrame implements ActionListener {
+        JPanel panel;
+        JLabel userLabel, passwordLabel, message;
+        JTextField userText;
+        JPasswordField passwordText;
+        JButton submit,cancel;
+
+        loginScreen() {
+            //username labels
+            userLabel = new JLabel();
+            userLabel.setText("Username: ");
+            userText = new JTextField();
+
+            //password labels
+            passwordLabel = new JLabel();
+            passwordLabel.setText("Password: ");
+            passwordText = new JPasswordField();
+
+            //Submit button
+            submit = new JButton("SUBMIT");
+            panel = new JPanel(new GridLayout(3, 1));
+            panel.add(userLabel);
+            panel.add(userText);
+            panel.add(passwordLabel);
+            panel.add(passwordText);
+            message = new JLabel();
+            panel.add(message);
+            panel.add(submit);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //adding listeners
+            submit.addActionListener(this);
+            add(panel,BorderLayout.CENTER);
+            setTitle("Please enter your login credentials!");
+            setSize(450,350);
+            setVisible(true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String userName = userText.getText();
+            String passWord = passwordText.getText();
+            if(userName.trim().equals("admin") && passWord.trim().equals("admin")) {
+                message.setText(" Hello "+ userName + "");
+            }
+            else {
+                message.setText(" Invalid user...");
+            }
+        }
+}
+
 
     public class projectMain {
         public static void main(String[] args) {
-            //code GUI stuff here
-            //not finished yet - Efaz
+            //creating login GUI
+            new loginScreen();
+
+            //code GUI stuff here,not finished yet - Efaz
             String[] degrees = new String[]{"Computer Science", "Nursing", "Mechanical"};
             JComboBox<String> comboDegree = new JComboBox<String>(degrees); //drop down box
             JPanel panel = new JPanel(); //just in case we need a panel
@@ -25,4 +80,4 @@ class selectListener implements ActionListener { //added this for the combo box 
             comboDegree.addActionListener(new selectListener());
             project.setVisible(true);
         }
-    }
+}
